cadc41f2bc7f6838549c956ea30277e3eb417bf6
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-11-05 15:42:14 -0500

    mobile menu animations and site sizing styles

--- mobile-menu.js
+++ mobile-menu.js
@@ -6,15 +6,32 @@ const mobileMenu = () => {
     var path2 = document.querySelector('.path2');
     var mline = document.querySelector('.mline');
 
+    var slideText1 = document.querySelector('.hero-section h2');
+    var slideText2 = document.querySelector('.hero-section h3');
+    var slideText3 = document.querySelector('.hero-section h5');
+
     var menu = document.querySelector('.mobile-menu');
 
+    // Default Button
     menuButton.addEventListener('click',() => {     
         path1.classList.toggle('cross');
         path2.classList.toggle('cross');
         mline.classList.toggle('hide');
+
+        textAnimations();
+
         menu.classList.toggle('open-menu');
+        
+        if (menu.classList.contains('mobile-menu-fade-in')) {
+            menu.classList.add('mobile-menu-fade-out');
+        } else {
+            menu.classList.remove('mobile-menu-fade-out')
+        }
+
+        menu.classList.toggle('mobile-menu-fade-in');
     });
 
+    // iOS Button
     iosButton.addEventListener('click',() => {
         if (iosButton.src.indexOf('icons/ios-mobile-menu.svg') > 0) {
             iosButton.src = 'icons/ios-close-menu.svg';
@@ -22,8 +39,28 @@ const mobileMenu = () => {
             iosButton.src = 'icons/ios-mobile-menu.svg'
         }
 
+        textAnimations();
+
         menu.classList.toggle('open-menu');
+
+        if (menu.classList == "mobile-menu-fade-in") {
+            menu.classList.add('mobile-menu-fade-out');
+        } else {
+            menu.classList.remove('mobile-menu-fade-out')
+        }
+
+        menu.classList.toggle('mobile-menu-fade-in');
     });
+
+    // Slide titles to side
+    function textAnimations() {
+        slideText1.classList.toggle('h2-slide');
+        slideText2.classList.toggle('h3-slide');
+        slideText3.classList.toggle('h5-slide');
+        slideText1.classList.toggle('fade-in-title');
+        slideText2.classList.toggle('fade-in-title');
+        slideText3.classList.toggle('fade-in-title');
+    }
 };
 
 mobileMenu();
